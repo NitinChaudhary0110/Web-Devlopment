5af1ee4d6612e0cb543af7c92957cf7404e56f84
NitinChaudhary0110 <chaudharynitin7535@gmail.com>
2025-07-30 13:45:19 +0530

    app.js

--- project1/app.js
+++ project1/app.js
@@ -1,28 +1,38 @@
-let inp=document.querySelector("input");
-let btn=document.querySelector("button");
-let ul=document.querySelector("ul");
+// Input, button, and unordered list elements ko select karna
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
 
+// Jab "Add Task" button pe click ho
+btn.addEventListener("click", function () {
+    // Ek naya <li> element banate hain
+    let list = document.createElement("li");
+    
+    // <li> ke andar input field ka text set karte hain
+    list.innerText = inp.value;
+    
+    // <ul> ke andar <li> add kar dete hain
+    ul.appendChild(list);
 
-btn.addEventListener("click",function(){
-let list=document.createElement("li");
-list.innerText=inp.value;
-ul.appendChild(list);
+    // Delete button create karte hain
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete"; // Button ka text set karna
+    delbtn.classList.add("button"); // CSS class add karna (agar style diya ho)
+    
+    // Delete button ko <li> ke andar lagate hain
+    list.appendChild(delbtn);
 
-let delbtn=document.createElement("button");
-delbtn.innerText="delete";
-delbtn.classList.add("button");
-list.appendChild(delbtn);
+    // Input field ko clear kar dete hain
+    inp.value = "";
+});
 
-inp.value="";
-
-})
-
-
-ul.addEventListener("click",function(event){
-    if(event.target.nodeName=="BUTTON"){
-        let par=event.target.parentElement;
-        par.remove();
-        console.log("task delete");
-        
+// Event delegation: pura <ul> pe click event lagaya gaya hai
+ul.addEventListener("click", function (event) {
+    // Agar click hua element ek BUTTON hai
+    if (event.target.nodeName == "BUTTON") {
+        // Uss button ka parent element (li) remove kar doo
+        let par = event.target.parentElement;
+        par.remove(); // Task delete karna
+        console.log("task delete"); 
     }
-})
+});
